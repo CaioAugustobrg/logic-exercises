a01a530614bb500fd14b831c10689d33c3b4f260
Caio Braga <caioaugustobrg@gmail.com>
2023-04-26 00:57:14 -0300

    :sparkles: feat: tomada exercise

---
+++ C/1aLista/tomada.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+int T1, T2, T3, T4;
+  T1 = 2 <= T1 <= 6;
+  T2 = 2 <= T2 <= 6;
+  T3 = 2 <= T3 <= 6;
+  T4 = 2 <= T4 <= 6;
+ scanf("%d %d %d %d",&T1,&T2,&T3,&T4);
+ printf("%d\n", T1 + T2 + T3 + T4 - 3 );
+return 0;
+};
