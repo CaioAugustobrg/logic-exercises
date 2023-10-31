f4b223eee0f47e063ca7620794175d4eaabc8c13
Caio Braga <caioaugustobrg@gmail.com>
2023-10-31 10:45:43 -0300

    :sparkles: feat: media ponderada py

---
+++ python/mediaPonderada.py
@@ -0,0 +1,9 @@
+x1 = float(input()) 
+x2 = float(input())
+x3 = float(input())
+x4 = float(input())
+x5 = float(input())
+media = (x1 + (x2 * 2) + (x3 * 3) + (x4 * 4) + (x5 * 5)) / 15
+
+
+print('{:.3f}'.format(media))
