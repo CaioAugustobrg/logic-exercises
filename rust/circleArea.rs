7756af6ad68a5e947b5466d1dfee2dc6d70ba15d
CaioAugustobrg <caioaugustobrg@gmail.com>
2025-04-22 19:48:33 -0300

    feat: exercise circle area

--- rust/circleArea.rs
+++ rust/circleArea.rs
@@ -1,9 +1,16 @@
 use std::io;
 
 fn main() {
-    // Escreva a sua solução aqui
-    // Code your solution here
-    // Escriba su solución aquí
+    let mut raio: String = String::new();
+    io::stdin()
+        .read_line(&mut raio)
+        .expect("FAILED TO READ INPUT");
 
-    
+    let raio: f32 = raio.trim().parse().expect("FAILED TO PARSE FLOAT");
+
+    let n: f32 = 3.14159;
+
+    let area: f32 = n * (raio * raio);
+
+    println!("A={}", area);
 }
