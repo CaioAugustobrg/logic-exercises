e859d7996bf11f0d845a02ddd26845bb3b7bb3ca
CaioAugustobrg <caioaugustobrg@gmail.com>
2025-08-02 12:58:00 -0300

    refactor: now with function

--- rust/src/bin/avarage_match.rs
+++ rust/src/bin/avarage_match.rs
@@ -3,40 +3,29 @@
 use std::io;
 
 fn main() {
-    let grade1: f64 = loop {
-        println!("TYPE FIRST NUMBER");
+    
+    println!("TYPE FIRST NUMBER");
+    let grade1: f64 = read_grade();
+
+    println!("TYPE SECOND NUMBER");
+    let grade2: f64 = read_grade();
+
+
+    println!("TYPE THIRD NUMBER");
+    let grade3: f64 = read_grade();
+    
+    println!("{}", (grade1 + grade2 + grade3) / 3.00);
+
+}
+
+
+fn read_grade() -> f64 {
+    loop {
         let mut input: String = String::new();
         io::stdin().read_line(&mut input).expect("Invalid number");
-
         match input.trim().parse() {
             Ok(grade) => break grade,
             Err(_) => println!("Invalid input. Try again"),
         }
-    };
-
-    let grade2: f64 = loop {
-        println!("TYPE SECOND NUMBER");
-        let mut input: String = String::new();
-        io::stdin().read_line(&mut input).expect("INVALID NUMBER");
-
-        match input.trim().parse() {
-            Ok(grade) => break grade,
-            Err(_) => println!("Invalid input. Try agaun"),
-        }
-    };
-
-    let grade3: f64 = loop {
-        println!("TYPE THIRD NUMBER");
-        let mut input: String = String::new();
-        io::stdin().read_line(&mut input).expect("INVALID NUMBER");
-
-        match input.trim().parse() {
-            Ok(grade) => break grade,
-            Err(_) => println!("Invalid input. Try agaun"),
-        }
-    };
-
-
-    println!("{}", (grade1 + grade2 + grade3) / 3.00);
-
+    }
 }
